931d0a0d9b8827b8e2ce027b3d5f2e5075b61b08
DizzyDogg <jeffthetenor@yahoo.com>
2018-11-08 15:21:37 -0700

    missed an underbar

--- src/scenes/loading.js
+++ src/scenes/loading.js
@@ -21,7 +21,7 @@ require('../../assets/images/redboy.png');
 require('../../assets/json/redboy.json');
 require('../../assets/images/shield.png');
 require('../../assets/json/shield.json');
-require('../../assets/images/brownbox.png');
+require('../../assets/images/brown_box.png');
 
 export default class LoadingScene extends Phaser.Scene {
     constructor (config, key = 'Loading') {
